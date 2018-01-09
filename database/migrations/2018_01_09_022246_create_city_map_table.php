a2306eeb8e1b86f2b78231be64d9e38df27549a6
fumengmeng <fmengum@gmail.com>
2018-01-09 14:34:18 +0800

    修改错误

--- database/migrations/2018_01_09_022246_create_city_map_table.php
+++ database/migrations/2018_01_09_022246_create_city_map_table.php
@@ -16,7 +16,7 @@ class CreateCityMapTable extends Migration
             $table->increments('id');
             $table->string('cityid')->comment('城市ID');
             $table->char('provinceid',1)->comment('省份ID');
-            $table->string('name',1)->comment('城市名');
+            $table->string('name')->comment('城市名');
             $table->string('ename')->comment('拼音');
             $table->char('eshort',2)->comment('简拼');
             $table->string('near')->comment('附近');
