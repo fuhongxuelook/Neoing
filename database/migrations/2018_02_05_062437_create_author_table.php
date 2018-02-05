9b6fca9d76307e004f433b204b51ce22ed3a6d9d
fumengmeng <fmengum@gmail.com>
2018-02-05 14:28:52 +0800

    修改错误

---
+++ database/migrations/2018_02_05_062437_create_author_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('auid',15)->comment('画家ID');
+            $table->string('describe')->comment('自我介绍');
+            $table->integer('state')->comment('账号状态');
+            $table->integer('auth')->comment('权限');
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('author');
+    }
+}
