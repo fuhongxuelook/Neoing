1f74e06c128b280a01572bac5667818cbda289bd
fumengmeng <fmengum@gmail.com>
2018-02-07 17:09:45 +0800

    增加model dialog

---
+++ app/Asset/Engine/Module/Home/IndexModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Neo\Asset\Engine\Module\Home;
+use Illuminate\Database\Eloquent\Model;
+
+//use Neo\Asset\Schema\Project\ProjectSchema as Schema;
+//use Neo\Asset\Bean\Project\ProjectBean;
+//use Neo\Asset\Config\ProjectConfig as Conf;
+//use Illuminate\Support\Facades\Storage;
+
+class IndexModel extends Eloquent {
+
+	function __construct() {
+	}
+
+    protected table = 'dialog';
+
+    public function dialogList() {
+    	
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
